5db45e854ac410d7c742189543190bf0d8774a4d
Yavar <y.t.azar@gmail.com>
2024-10-16 11:41:16 +0200

    first commit

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Catalysts'
+copyright = '2024, Yavar'
+author = 'Yavar'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',  # Add this line
+    'sphinx.ext.mathjax',  # Assuming you have this and possibly other extensions
+    'sphinx_book_theme',]
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
---
+++ docs/intro/introduction.rst
@@ -0,0 +1,13 @@
+Catalyst Design: DFT & Machine Learning Approaches
+=============================================================
+
+This repository collects my personal experience and knowledge on catalyst design, 
+with a focus on computational methods such as Density Functional Theory (DFT) 
+and machine learning applications. 
+
+It includes insights into catalyst mechanism elucidation, optimization strategies, 
+and the integration of machine learning models to predict catalyst behavior, 
+as highlighted in state-of-the-art research articles. 
+
+This resource is aimed at streamlining the design process and enhancing the 
+understanding of catalyst systems for both theoretical and practical applications.
